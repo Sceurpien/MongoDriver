a551ac3aab2a4b2a608e9d0fb4c7034f5f5de03d
Gabor Imre Bata-Kovacs <gaborimre.bata-kovacs@dealogic.com>
2019-09-27 05:09:30 +0200

    MongoDriver

---
+++ Models/DBContext.cs
@@ -0,0 +1,34 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MongoDriver.Models
+{
+    class DbContext
+    {
+        public const string CONNECTION_STRING_NAME = "drivertest";
+        public const string DATABASE_NAME = "drivertest";
+        public const string ITEMS_COLLECTION_NAME = "items";
+
+        private static readonly IMongoClient _client;
+        private static readonly IMongoDatabase _database;
+
+        static DbContext()
+        {
+            var connectionString = "mongodb+srv://dealogic_mongo:W4RAXDsy18bBFvNM@dealogicsamplecluster-xtf3z.azure.mongodb.net/test?retryWrites=true&w=majority";
+            _client = new MongoClient(connectionString);
+            _database = _client.GetDatabase(DATABASE_NAME);
+        }
+
+        public IMongoClient Client
+        {
+            get { return _client; }
+        }
+
+        public IMongoCollection<Item> Items
+        {
+            get { return _database.GetCollection<Item>(ITEMS_COLLECTION_NAME); }
+        }
+    }
+}
---
+++ Models/SubItem.cs
@@ -0,0 +1,17 @@
+﻿
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+
+namespace MongoDriver.Models
+{
+    public class SubItem
+    {
+        [BsonElement("name")]
+        public string Name { get; set; }
+
+        [BsonElement("strings")]
+        public List<string> Strings { get; set; }
+    }
+}
